9b485bce2049414f04b9b8645ab9dc573a295335
Muhammad Shahid <contacttoshahidkhan@gmail.com>
2025-11-25 22:35:32 +0500

    Initial Commit

---
+++ case/caseApp.swift
@@ -0,0 +1,17 @@
+//
+//  caseApp.swift
+//  case
+//
+//  Created by Shahid's MacPro on 25/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct caseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
